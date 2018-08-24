927fd935c181cac0aadc87eac3d50735b6a4eb6c
zmilonas <25948390+zmilonas@users.noreply.github.com>
2018-08-24 19:33:04 +0200

    Change generation and deployment scripts to ease deployment to wordpress template

--- README.md
+++ README.md
@@ -2,23 +2,35 @@
 
 http://www.internetjestdlaludzi.pl
 
+https://centrumcyfrowe.pl/reforma
+
 ## Builiding locally
 
+If you're using yarn, like the original author - @zmilonas - replace all `npm` and `npm run` with `yarn`.
+
 1. Install dependencies
-```bash
-npm install 
-#or
-yarn install
+```sh
+npm install
 ```
 
-2. Run the bundled style and script generation script
-```bash
-node_modules/.bin/gulp
-#or to run continously 
-node_modules/.bin/gulp watch
+2. Run the bundled style and script generation script and start the webserver
+```sh
+npm run start
 ```
 
-3. Start a local webserver in the root repo folder. For example:
+*The local copy of the site should be available at the address printed in the console - default is http://localhost:8080*
+
+### For active development
+
+1. Run the watcher on the styles and the scripts, while running the server
+```sh
+npm run watch
 ```
-php -S localhost:8080
+
+## Deployment
+
+```sh
+npm run deploy
 ```
+
+copy all generated files from `dist/` to remote location.
--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,59 @@ const minifycss = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
+const replace = require('gulp-replace');
+const rename = require('gulp-rename');
+const package = require('./package.json');
 
 const config = {
   input: {
     css: [
-      'src/css/*.css',
-      'src/css/scss/*.scss',
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      'src/scss/*.scss',
     ],
     js: [
-      'src/js/jquery.js',
+      'node_modules/jquery/dist/jquery.js',
       'src/js/*.js',
     ],
+    html: 'index.html',
   },
   output: {
-    folder: 'dist',
-    css: 'style.css',
-    js: 'bundle.js',
+    folder: {
+      js: 'dist/js',
+      css: 'dist/css',
+      html: 'dist'
+    },
+    css: `${package.name}.css`,
+    js: `${package.name}.js`,
+    html: `template-${package.name}.php`,
   },
-  options: {
-    css: {
-      outputStyle: 'expanded'
+  sassOptions: {
+    dev: {
+      outputStyle: 'expanded',
+    },
+    prod: {
+      outputStyle: 'compressed',
     }
   }
+}
+const livedist = 'https://centrumcyfrowe.pl/wp-content/themes/cc2018/dist/';
+const replacements = {
+  assets: ['/assets/', livedist + 'images/' + package.name + '/'],
+  depend: ['dist/', livedist],
+  footer: ['<footer></footer>','<?php get_footer(); ?>'],
+  header: ['<!',`<?php
+/*
+ * Template name: Internet jest dla ludzi landing
+ */
+?><!`],
 }
 
 gulp.task('build-css', () => {
   return gulp
     .src(config.input.css)
-    .pipe(sass(config.options.css).on('error', sass.logError))
+    .pipe(sass(config.sassOptions.dev).on('error', sass.logError))
     .pipe(concat(config.output.css))
-    .pipe(minifycss())
-    .pipe(gulp.dest(config.output.folder));
+    .pipe(gulp.dest(config.output.folder.css));
 });
 
 gulp.task('build-js', () => {
@@ -43,10 +65,28 @@ gulp.task('build-js', () => {
     .pipe(concat(config.output.js))
     .pipe(babel({'presets': ['env']}).on('error', console.error.bind(console)))
     .pipe(uglify())
-    .pipe(gulp.dest(config.output.folder));
+    .pipe(gulp.dest(config.output.folder.js));
 });
-
-gulp.task('watch', () => {
+gulp.task('prod-css', () => {
+  return gulp
+    .src(config.input.css)
+    .pipe(sass(config.sassOptions.prod).on('error', sass.logError))
+    .pipe(replace(replacements.assets[0], replacements.assets[1]))
+    .pipe(concat(config.output.css))
+    .pipe(minifycss())
+    .pipe(gulp.dest(config.output.folder.css));
+});
+gulp.task('deploy', ['prod-css', 'build-js'], () => {
+  return gulp
+    .src(config.input.html)
+    .pipe(rename(config.output.html))
+    .pipe(replace(replacements.depend[0], replacements.depend[1]))
+    .pipe(replace(replacements.assets[0], replacements.assets[1]))
+    .pipe(replace(replacements.header[0], replacements.header[1]))
+    .pipe(replace(replacements.footer[0], replacements.footer[1]))
+    .pipe(gulp.dest(config.output.folder.html))
+})
+gulp.task('watch', ['build-css', 'build-js'], () => {
   gulp.watch(config.input.css, ['build-css']);
   gulp.watch(config.input.js, ['build-js']);
 });
