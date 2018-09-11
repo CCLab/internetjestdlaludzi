ddff64bb284283c08c0f0c8dd61da925eb5363f5
zmilonas <25948390+zmilonas@users.noreply.github.com>
2018-09-12 00:26:19 +0200

    Add recent-news, enable cachebusting deploys

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,12 @@ const config = {
   input: {
     css: [
       'node_modules/bootstrap/scss/bootstrap.scss',
+      'node_modules/swiper/dist/css/swiper.min.css',
       'src/scss/*.scss',
     ],
     js: [
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/swiper/dist/js/swiper.min.js',
       'src/js/*.js',
     ],
     html: 'index.html',
@@ -26,8 +28,8 @@ const config = {
       css: 'dist/css',
       html: '.'
     },
-    css: `${package.name}2.css`,
-    js: `${package.name}2.js`,
+    css: `${package.name}.css`,
+    js: `${package.name}.js`,
     html: `template.php`,
   },
   sassOptions: {
@@ -40,6 +42,15 @@ const config = {
   }
 }
 const livedist = 'https://centrumcyfrowe.pl/wp-content/themes/cc2018/dist/';
+
+const fullpath = type => config.output.folder[type] + '/' + config.output[type];
+const deployConstant = Math.random().toString(36).slice(-5);
+const deployRandName = {
+  css: `${package.name}-${deployConstant}.css`,
+  js: `${package.name}-${deployConstant}.js`,
+}
+const deployPath = type => config.output.folder[type] + '/' + deployRandName[type];
+
 const replacements = {
   assets: ['/assets/', livedist + 'images/' + package.name + '/'],
   depend: ['dist/', livedist],
@@ -48,7 +59,7 @@ const replacements = {
 /*
  * Template name: Internet jest dla ludzi landing
  */
-?><!doctype`],
+?><!doctype`]
 }
 
 gulp.task('build-css', () => {
@@ -67,16 +78,22 @@ gulp.task('build-js', () => {
     .pipe(uglify())
     .pipe(gulp.dest(config.output.folder.js));
 });
+gulp.task('prod-js', ['build-js'], () => {
+  return gulp
+    .src(fullpath('js'))
+    .pipe(rename(deployRandName.js))
+    .pipe(gulp.dest(config.output.folder.js));
+});
 gulp.task('prod-css', () => {
   return gulp
     .src(config.input.css)
     .pipe(sass(config.sassOptions.prod).on('error', sass.logError))
     .pipe(replace(replacements.assets[0], replacements.assets[1]))
-    .pipe(concat(config.output.css))
+    .pipe(concat(deployRandName.css))
     .pipe(minifycss())
     .pipe(gulp.dest(config.output.folder.css));
 });
-gulp.task('deploy', ['prod-css', 'build-js'], () => {
+gulp.task('deploy', ['prod-css', 'prod-js'], () => {
   return gulp
     .src(config.input.html)
     .pipe(rename(config.output.html))
@@ -84,6 +101,8 @@ gulp.task('deploy', ['prod-css', 'build-js'], () => {
     .pipe(replace(replacements.assets[0], replacements.assets[1]))
     .pipe(replace(replacements.header[0], replacements.header[1]))
     .pipe(replace(replacements.footer[0], replacements.footer[1]))
+    .pipe(replace(fullpath('js'), deployPath('js')))
+    .pipe(replace(fullpath('css'), deployPath('css')))
     .pipe(gulp.dest(config.output.folder.html))
 })
 gulp.task('watch', ['build-css', 'build-js'], () => {
